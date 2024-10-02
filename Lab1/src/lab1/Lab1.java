c800b4dfa3864694280238d2bf312082b1f7ae41
RRuiz1998 <rruiz10437@ufide.ac.cr>
2024-10-02 15:28:38 -0600

    Add files via upload

---
+++ Lab1/src/lab1/Lab1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab1;
+
+/**
+ *
+ * @author Lenovo T440p
+ */
+public class Lab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int x = 3;
+        int y = 5;
+        
+        int z = x+y;
+        
+        System.out.println(z);
+    }
+    
+}
